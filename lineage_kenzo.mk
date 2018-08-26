ab6eded30875c869daa8788c293127b7bb307431
Amitava Mitra <mitraamitava7@gmail.com>
2018-08-26 04:51:56 +0000

    Lineageify

--- lineage_kenzo.mk
+++ lineage_kenzo.mk
@@ -17,8 +17,8 @@
 
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
-# Inherit some common candy stuff.
-$(call inherit-product, vendor/candy/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Bootanimation
 TARGET_SCREEN_HEIGHT := 1080
@@ -28,7 +28,7 @@ TARGET_SCREEN_WIDTH := 1920
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := candy_kenzo
+PRODUCT_NAME := lineage_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
